b7f28ab144095839853f0c8fe86b518dbd77ce4e
st10484188-Luc-Bertillon <st10484188@vcconnect.edu.za>
2025-04-02 11:50:45 +0200

    fixed error and created exit button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         val txtView = findViewById<TextView>(R.id.TxtView)
         val edtTime = findViewById<EditText>(R.id.EdtTime)
         val btnGenerate = findViewById<Button>(R.id.btnGenerate)
+        val btnExit = findViewById<Button>(R.id.btnExit)
 
 
         btnGenerate.setOnClickListener {
@@ -35,31 +36,41 @@ class MainActivity : AppCompatActivity() {
                 "morning" -> {
                     meals = "Breakfast: Eggs"
                 }
+
                 "mid-morning" -> {
                     meals = "Snack: Fruit"
                 }
+
                 "afternoon" -> {
                     meals = "Lunch: Sandwich"
                 }
+
                 "mid-afternoon" -> {
                     meals = "Snack: Cake"
                 }
+
                 "dinner" -> {
                     meals = "Main Course: Pasta"
                 }
+
                 "after dinner" -> {
                     meals = "Dessert: Ice cream"
                 }
+
                 else -> {
                     meals = "Please enter Text!"
                     Toast.makeText(this, "Please enter text", Toast.LENGTH_SHORT).show()
                 }
             }
-           txtView.text = meals
+            txtView.text = meals
         }
-        btnReset.setOnClickListener{
+        btnReset.setOnClickListener {
             txtView.text = ""
             edtTime.text.clear()
             Toast.makeText(this, "Cleared", Toast.LENGTH_SHORT).show()
+        }
+        btnExit.setOnClickListener{
+            finish()
+        }
     }
 }
